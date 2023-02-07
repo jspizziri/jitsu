62acc1b50d9322a5cf712a753b19354254c87cc0
Vladimir Klimontovich <vladimir@jitsu.com>
2023-02-07 17:59:10 -0500

    fix (ui): make jitsu tracking optional

--- configurator/frontend/main/src/App.tsx
+++ configurator/frontend/main/src/App.tsx
@@ -1,5 +1,5 @@
 // @Libs
-import React, { ComponentType, ExoticComponent, useEffect, useState } from "react"
+import React, { ComponentType, ExoticComponent, PropsWithChildren, useEffect, useState } from "react"
 import { Redirect, Route, Switch, useLocation, NavLink } from "react-router-dom"
 import { Button, Card, Typography } from "antd"
 import { useParams } from "react-router"
@@ -117,6 +117,20 @@ const initializeProject = async (
   return project
 }
 
+//Wraps the whole application with the JitsuProvider if jitsu host is configured
+export const JitsuWrapper: React.FC<PropsWithChildren<{host?: string, identifyHook?: any}>> = ({host, identifyHook, children}) => {
+  if (!host) {
+    return <>{children}</>
+  } else {
+    return (
+      <JitsuProvider options={{ host, autoPageTracking: { reactRouter: useLocation }, before: identifyHook }}>
+        {children}
+      </JitsuProvider>
+    )
+  }
+
+}
+
 export const Application: React.FC = function () {
   const [services, setServices] = useState<ApplicationServices>()
   const [projects, setProjects] = useState<Project[]>(null)
@@ -206,9 +220,7 @@ export const Application: React.FC = function () {
   if (!services.userService.hasUser()) {
     return (
       <React.Suspense fallback={<CenteredSpin />}>
-        <JitsuProvider
-          options={{ host: jitsuHost, autoPageTracking: { reactRouter: useLocation }, before: identifyHook }}
-        >
+        <JitsuWrapper host={jitsuHost} identifyHook={identifyHook}>
           {services.showSelfHostedSignUp() && <SetupForm />}
           {!services.showSelfHostedSignUp() && (
             <Switch>
@@ -239,16 +251,14 @@ export const Application: React.FC = function () {
               <Redirect to="/" />
             </Switch>
           )}
-        </JitsuProvider>
+        </JitsuWrapper>
       </React.Suspense>
     )
   }
 
   return (
     <>
-      <JitsuProvider
-        options={{ host: jitsuHost, autoPageTracking: { reactRouter: useLocation }, before: identifyHook }}
-      >
+      <JitsuWrapper host={jitsuHost} identifyHook={identifyHook}>
         <Switch>
           <Route
             path={"/user/settings"}
@@ -274,7 +284,7 @@ export const Application: React.FC = function () {
             <ProjectRedirect projects={projects} />
           </Route>
         </Switch>
-      </JitsuProvider>
+      </JitsuWrapper>
     </>
   )
 }
